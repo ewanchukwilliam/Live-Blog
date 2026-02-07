cc10e0dc78bcc4db0f8225ce32dbd303eb88d355
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-06 23:27:53 -0700

    frontend almost complete just needs content to fill with.

--- Blog-Frontend/app/app.config.ts
+++ Blog-Frontend/app/app.config.ts
@@ -22,7 +22,7 @@ export default defineAppConfig({
 			base: "flex flex-col gap-8 lg:gap-y-16",
 			variants: {
 				orientation: {
-					horizontal: "sm:grid sm:grid-cols-2 lg:grid-cols-3 px-20 py-2",
+					horizontal: "sm:grid sm:grid-cols-2 lg:grid-cols-3 py-2",
 					vertical: "",
 				},
 			},
