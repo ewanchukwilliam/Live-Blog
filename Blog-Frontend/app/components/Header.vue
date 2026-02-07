19635171e886eb5f1ea9e4f1917e603b1dcf7b97
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-06 19:02:59 -0700

    nicer navigation menu

--- Blog-Frontend/app/components/Header.vue
+++ Blog-Frontend/app/components/Header.vue
@@ -1,40 +1,9 @@
-<script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui'
-
-const route = useRoute()
-
-const items = computed<NavigationMenuItem[]>(() => [
-  {
-    label: 'Docs',
-    to: '/docs/getting-started',
-    active: route.path.startsWith('/docs/getting-started')
-  },
-  {
-    label: 'Components',
-    to: '/docs/components',
-    active: route.path.startsWith('/docs/components')
-  },
-  {
-    label: 'Figma',
-    to: 'https://go.nuxt.com/figma-ui',
-    target: '_blank'
-  },
-  {
-    label: 'Releases',
-    to: 'https://github.com/nuxt/ui/releases',
-    target: '_blank'
-  }
-])
-</script>
-
 <template>
   <UHeader>
     <template #title>
       <Logo class="h-6 w-auto" />
     </template>
-
-    <UNavigationMenu :items="items" />
-
+	<NavigationMenu/>
     <template #right>
       <UColorModeButton />
 
