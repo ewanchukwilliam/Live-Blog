8eeb133b5806e1a92d367a905b54ef5307fe9295
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-07 03:29:38 -0700

    added hydration friendly ssr images

--- Blog-Frontend/app/components/Posts.vue
+++ Blog-Frontend/app/components/Posts.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 const blogs = await useBlogs()
 
+const getFallbackImage = (path: string) => {
+  const num = path.match(/post-(\d+)/)?.[1] || '1'
+  return `/images/blog/post-${num}.jpg`
+}
+
 const posts = computed(() =>
   blogs.value?.map(blog => ({
     title: blog.title || 'Untitled',
     description: blog.description || '',
-    image: blog.image || `https://picsum.photos/seed/${blog.path.replace(/\//g, '-')}/800/400`,
+    image: blog.image || getFallbackImage(blog.path),
     date: blog.date || '',
     to: blog.path
   })) || []
