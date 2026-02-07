7746773e2fcd1f7bd3266ce0e55ff696854fb9fe
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-07 03:54:17 -0700

    added new emoji favicon

--- Blog-Frontend/nuxt.config.ts
+++ Blog-Frontend/nuxt.config.ts
@@ -34,6 +34,13 @@ export default defineNuxtConfig({
 		}
 	},
 	devtools: { enabled: true },
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🖥️</text></svg>' }
+			]
+		}
+	},
 	content: {
 		database: {
 			type: 'sqlite',
