ea0fe59364dfe2e34dca08943c298c2202351513
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-07 04:02:00 -0700

    added metadata for url

--- Blog-Frontend/nuxt.config.ts
+++ Blog-Frontend/nuxt.config.ts
@@ -36,6 +36,7 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	app: {
 		head: {
+			title: "William's Blog",
 			link: [
 				{ rel: 'icon', href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🖥️</text></svg>' }
 			]
