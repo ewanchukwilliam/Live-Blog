dfdee0871a46fbec007ae0e865ad61e5d43ee989
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-17 01:17:47 -0700

    fixed header doing weird navigation on blog color changes.

--- Blog-Frontend/app/components/Header.vue
+++ Blog-Frontend/app/components/Header.vue
@@ -1,14 +1,12 @@
 <template>
   <UHeader>
-    <template #title>
-      <Logo class="h-6 w-auto" />
-      <UColorModeButton />
-    </template>
+    <template #title> Williams Blog </template>
     <NavigationMenu />
     <template #right>
+      <UColorModeButton />
       <AccountState />
     </template>
-      <!-- This is for the Collapsed Hamburger menu items on the mobile view -->
+    <!-- This is for the Collapsed Hamburger menu items on the mobile view -->
     <template #body>
       <NavigationMenu orientation="vertical" class="w-full" />
     </template>
