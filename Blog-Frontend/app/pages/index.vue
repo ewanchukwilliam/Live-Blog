9b02c974e07e2097dde199e2226e49998aeed53d
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-19 19:23:25 -0700

    cleaner more readable paragraphs

--- Blog-Frontend/app/pages/index.vue
+++ Blog-Frontend/app/pages/index.vue
@@ -46,139 +46,141 @@ const links = ref<ContentTocLink[]>([
       </UPageAside>
     </template>
 
-    <UPageBody>
-      <section
-        class="scroll-mt-20"
-        v-motion
-        :initial="{ opacity: 0, y: 100 }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: { delay: 400, duration: 1000, easing: 'easeOut' },
-        }"
-      >
-        <h2 id="latest-posts" class="text-2xl font-bold">Latest Posts</h2>
-        <p class="text-muted mb-2">Check out my latest posts and articles.</p>
-        <Posts />
-      </section>
+		<div class="min-w-full flex flex-col items-center">
+			<UPageBody class="max-w-4xl">
+				<section
+					class="scroll-mt-20"
+					v-motion
+					:initial="{ opacity: 0, y: 100 }"
+					:enter="{
+						opacity: 1,
+						y: 0,
+						transition: { delay: 400, duration: 1000, easing: 'easeOut' },
+					}"
+				>
+					<h2 id="latest-posts" class="text-2xl font-bold">Latest Posts</h2>
+					<p class="text-muted mb-2">Check out my latest posts and articles.</p>
+					<Posts />
+				</section>
 
-      <section
-        class="scroll-mt-20"
-        v-motion
-        :initial="{ opacity: 0, y: 100 }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: { delay: 600, duration: 1000, easing: 'easeOut' },
-        }"
-      >
-        <h2 id="introduction" class="text-2xl font-bold scroll-mt-20">
-          Introduction
-        </h2>
-        <p class="text-muted">
-          I enjoy developing my skills through projects. I'm looking to benefit
-          from higher quality documentation of my progress as a developer. I'm
-          looking to rely on LLM's less that I currently do in my day to day and
-          this is my way of documenting my journey along side the evolution of
-          AI in hopes that in the future as a software engineer maybe some
-          famous anti-ai influencer benefits from my experience :), who know's
-          maybe someday I'll realize my mistakes and never write another line of
-          code.
-        </p>
+				<section
+					class="scroll-mt-20"
+					v-motion
+					:initial="{ opacity: 0, y: 100 }"
+					:enter="{
+						opacity: 1,
+						y: 0,
+						transition: { delay: 600, duration: 1000, easing: 'easeOut' },
+					}"
+				>
+					<h2 id="introduction" class="text-2xl font-bold scroll-mt-20">
+						Introduction
+					</h2>
+					<p class="text-muted">
+						I enjoy developing my skills through projects. I'm looking to benefit
+						from higher quality documentation of my progress as a developer. I'm
+						looking to rely on LLM's less that I currently do in my day to day and
+						this is my way of documenting my journey along side the evolution of
+						AI in hopes that in the future as a software engineer maybe some
+						famous anti-ai influencer benefits from my experience :), who know's
+						maybe someday I'll realize my mistakes and never write another line of
+						code.
+					</p>
 
-        <div class="mt-6">
-          <h3 id="about-me" class="text-xl font-semibold scroll-mt-20">
-            About Me
-          </h3>
-          <p class="text-muted">
-            I'm a Computer Engineering Student making a late in life career
-            pivot. I'm in my 3rd year of uni at the University of Alberta. I
-            come from an industrial medic background. I'm pursuing a childish
-            dream of mind years ago in the new softare engineering direction.
-            I'm personally interested in both Full Stack Development and Devops.
-            I like the scalability of distributed systems, and developing
-            applications from scratch. I use Neovim, and have a custom terminal
-            inspired workflow I've come to love.
-          </p>
-        </div>
+					<div class="mt-6">
+						<h3 id="about-me" class="text-xl font-semibold scroll-mt-20">
+							About Me
+						</h3>
+						<p class="text-muted">
+							I'm a Computer Engineering Student making a late in life career
+							pivot. I'm in my 3rd year of uni at the University of Alberta. I
+							come from an industrial medic background. I'm pursuing a childish
+							dream of mind years ago in the new softare engineering direction.
+							I'm personally interested in both Full Stack Development and Devops.
+							I like the scalability of distributed systems, and developing
+							applications from scratch. I use Neovim, and have a custom terminal
+							inspired workflow I've come to love.
+						</p>
+					</div>
 
-        <div class="mt-6">
-          <h3 id="my-stack" class="text-xl font-semibold scroll-mt-20">
-            My Stack's
-          </h3>
-          <h2>Frameworks:</h2>
-          <p class="text-muted">
-            Kubernetes, Docker, Django, React, Next/Nuxts, Express, Springboot
-          </p>
-          <h2>Languages:</h2>
-          <p class="text-muted">
-            Python, Typescript, Java, (Go in the future someday)
-          </p>
-        </div>
-      </section>
+					<div class="mt-6">
+						<h3 id="my-stack" class="text-xl font-semibold scroll-mt-20">
+							My Stack's
+						</h3>
+						<h2>Frameworks:</h2>
+						<p class="text-muted">
+							Kubernetes, Docker, Django, React, Next/Nuxts, Express, Springboot
+						</p>
+						<h2>Languages:</h2>
+						<p class="text-muted">
+							Python, Typescript, Java, (Go in the future someday)
+						</p>
+					</div>
+				</section>
 
-      <section
-        class="scroll-mt-20"
-        v-motion
-        :initial="{ opacity: 0, y: 100 }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: { delay: 800, duration: 1000, easing: 'easeOut' },
-        }"
-      >
-        <h2 id="getting-started" class="text-2xl font-bold scroll-mt-20">
-          Getting Started
-        </h2>
-        <p class="text-muted">Explore the site and find what you need.</p>
+				<section
+					class="scroll-mt-20"
+					v-motion
+					:initial="{ opacity: 0, y: 100 }"
+					:enter="{
+						opacity: 1,
+						y: 0,
+						transition: { delay: 800, duration: 1000, easing: 'easeOut' },
+					}"
+				>
+					<h2 id="getting-started" class="text-2xl font-bold scroll-mt-20">
+						Getting Started
+					</h2>
+					<p class="text-muted">Explore the site and find what you need.</p>
 
-        <div class="mt-6">
-          <h3 id="quick-links" class="text-xl font-semibold scroll-mt-20">
-            Quick Links
-          </h3>
-          <div class="flex flex-wrap gap-2">
-            <UButton
-              icon="i-lucide-rocket"
-              size="md"
-              color="primary"
-              variant="solid"
-              to="/blogs/"
-            >
-              Blogs
-            </UButton>
-            <UButton
-              icon="i-lucide-user"
-              size="md"
-              color="primary"
-              variant="solid"
-              to="/about/"
-            >
-              About
-            </UButton>
-            <UButton
-              icon="i-lucide-mail"
-              size="md"
-              color="primary"
-              variant="solid"
-              to="/contact/"
-            >
-              Contact
-            </UButton>
+					<div class="mt-6">
+						<h3 id="quick-links" class="text-xl font-semibold scroll-mt-20">
+							Quick Links
+						</h3>
+						<div class="flex flex-wrap gap-2">
+							<UButton
+								icon="i-lucide-rocket"
+								size="md"
+								color="primary"
+								variant="solid"
+								to="/blogs/"
+							>
+								Blogs
+							</UButton>
+							<UButton
+								icon="i-lucide-user"
+								size="md"
+								color="primary"
+								variant="solid"
+								to="/about/"
+							>
+								About
+							</UButton>
+							<UButton
+								icon="i-lucide-mail"
+								size="md"
+								color="primary"
+								variant="solid"
+								to="/contact/"
+							>
+								Contact
+							</UButton>
 
 
-        <!-- TODO: Remember to clean up this 404 error please -->
-            <UButton
-              icon="i-lucide-alert-triangle"
-              size="md"
-              color="error"
-              variant="outline"
-              to="/temp/"
-            >
-              Test 404
-            </UButton>
-          </div>
-        </div>
-      </section>
-    </UPageBody>
+							<!-- TODO: Remember to clean up this 404 error please -->
+							<UButton
+								icon="i-lucide-alert-triangle"
+								size="md"
+								color="error"
+								variant="outline"
+								to="/temp/"
+							>
+								Test 404
+							</UButton>
+						</div>
+					</div>
+				</section>
+			</UPageBody>
+		</div>
   </UPage>
 </template>
